d7d838fcddfcd5fd5ed87e2186c01dac9c7ec24a
lalala <1213273985@qq.com>
2019-06-26 21:36:50 +0800

    加了人才招聘和关于我们

---
+++ dong/src/components/Talent.vue
@@ -0,0 +1,296 @@
+<template>
+  <div>
+    <div class="banner">
+      <dl>
+        <dd></dd>
+        <dt>人才招聘</dt>
+        <dd></dd>
+      </dl>
+    </div>
+    <div class="content">
+      <h3>人才招聘</h3>
+      <div class="content-sub">
+        <div class="sub1">
+          <h5>国际交流中心主任</h5>
+          <p>招聘人数&nbsp;1</p>
+        </div>
+        <div class="sub2">
+          <p>
+            <strong>岗位职责:</strong>
+            <br>
+            1、通过赛伯乐国际的全球投资产业布局，结合ZIII对接全球高校科研资源；
+            <br>
+            2、负责ZIII国际交流与合作平台及有效运营和维护；
+            <br>
+            3、负责国际交流活动策划组织及资助，组织创投人才国际化培训与交流；
+            <br>
+            4、负责国际交流资源整合、培育、维护和增值；
+            <br>
+            5、督导国际交流中心的各项经营和考核指标的完成情况，并及时提出解决问题的建议，以提高本中心业务能力。
+          </p>
+          <p>
+            <strong>岗位要求:</strong>
+            <br>
+            1、硕士及以上学历，工作经历5年以上，3年以上同类型工作经验；有海外工作经验者优先，浙大校友优先；
+            <br>
+            2、熟悉院校内部流程，具有较强的组织协调能力和管理决策能力；具有较强的跨部门协调、沟通能力与团队合作精神；
+            <br>
+            3、认同浙大文化精神，对国际交流项目有深刻认识。
+          </p>
+          <p>
+            <strong>简历投递邮箱:</strong>
+            <br>
+            <el-link href="https://www.baidu.com"
+                     target="_blank">lei.deng@cybernaut.com.cn</el-link>
+          </p>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="content-sub">
+        <div class="sub1">
+          <h5>国际交流中心助理</h5>
+          <p>招聘人数&nbsp;2</p>
+        </div>
+        <div class="sub2">
+          <p>
+            <strong>岗位职责:</strong>
+            <br>
+            1、国际教育与培训项目前期组织和配合工作；
+            <br>
+            2、整合赛伯乐国际教育和国际并购资源，与国外大学合作办学的具体工作对接和推进；
+            <br>
+            3、对国际课程的研发和推广工作的具体事务对接；
+            <br>
+            4、跟踪和了解政府和企业的培训需求，量身定制系列课程；
+            <br>
+            5、其他国际教育中心主任交办的工作。
+          </p>
+          <p>
+            <strong>岗位要求:</strong>
+            <br>
+            1、本科及以上学历，企业管理、英文、文秘、相关专业本科以上学历；
+            <br>
+            2、有海外教育培训工作实习经验者优先，较强的责任心和执行能力；
+            <br>
+            3、思路清晰，条理清楚，领悟力强，综合素质好；
+            <br>
+            4、形象端正、善于沟通协调，有亲和力及团队合作精神。
+          </p>
+          <p>
+            <strong>简历投递邮箱:</strong>
+            <br>
+            <el-link href="https://www.baidu.com"
+                     target="_blank">lei.deng@cybernaut.com.cn</el-link>
+          </p>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="content-sub">
+        <div class="sub1">
+          <h5>基金经理</h5>
+          <p>招聘人数&nbsp;1</p>
+        </div>
+        <div class="sub2">
+          <p>
+            <strong>岗位职责:</strong>
+            <br>
+            1、负责制订基金募集方案，根据基金募集方案进行项目路演、LP募集，及合伙人维护等；
+            <br>
+            2、基金管理：统筹管理基金的设立及运营等；
+            <br>
+            3、项目投资管理：能够独立开展投资工作，找寻项目投资机会，开展尽职调查及行业研究，制定项目投资计划，撰写项目尽职调查报告、投资建议书，并参与投资谈判、合同或协议的起草、修订等工作；
+            <br>
+            4、参与团队建设
+          </p>
+          <p>
+            <strong>岗位要求:</strong>
+            <br>
+            1、硕士及以上学历,金融、法律相关专业；
+            <br>
+            2、3年以上股权投资相关经验；
+            <br>
+            3、至少3个以上全程参与的项目经验。
+          </p>
+          <p>
+            <strong>简历投递邮箱:</strong>
+            <br>
+            <el-link href="https://www.baidu.com"
+                     target="_blank">lei.deng@cybernaut.com.cn</el-link>
+          </p>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="content-sub">
+        <div class="sub1">
+          <h5>投资助理</h5>
+          <p>招聘人数&nbsp;2</p>
+        </div>
+        <div class="sub2">
+          <p>
+            <strong>岗位职责:</strong>
+            <br>
+            1、在投资总监的指导下进行项目文件的起草、制作；
+            <br>
+            2、负责准备投委会材料和其他相关材料；
+            <br>
+            3、收集和整理各类宏观经济、行业及上市公司新闻，并对数据进行统计分析；
+            <br>
+            4、参与公司投资、融资、并购等重要经营活动的准备工作，并处理相关法律事务；
+            <br>
+            5、配合投资总监对投融资项目进行投资方案设计、尽职调查、投资管理；
+            <br>
+            6、协助投资总监对商务合同前期调查，对项目推进调研和资料收集，要有强烈的风险防控意识；
+            <br>
+            7、协助投资总监做好控制、防范公司营业风险，增强风险防控意识和风险救济技能。
+          </p>
+          <p>
+            <strong>岗位要求:</strong>
+            <br>
+            1、本科及以上学历，法律、金融、医学专业本科以上学历；
+            <br>
+            2、极强的学习能力，有较强的沟通能力；
+            <br>
+            3、有团队合作精神和高效执行力；
+            <br>
+            4、具备较强的文案撰写能力，较强的收集资料及分析能力；
+            <br>
+            5、精通PPT制作与Word等其他办公软件的应用。
+          </p>
+          <p>
+            <strong>简历投递邮箱:</strong>
+            <br>
+            <el-link href="https://www.baidu.com"
+                     target="_blank">lei.deng@cybernaut.com.cn</el-link>
+          </p>
+        </div>
+      </div>
+      <el-divider></el-divider>
+      <div class="content-sub">
+        <div class="sub1">
+          <h5>行政助理</h5>
+          <p>招聘人数&nbsp;2</p>
+        </div>
+        <div class="sub2">
+          <p>
+            <strong>岗位职责:</strong>
+            <br>
+            1、协助公司制订完善、组织实施人力资源管理有关规章制度和工作流程；
+            <br>
+            2、协助公司行政事务工作及部门内部日常事务工作；
+            <br>
+            3、办公用品、耗材用品等验收、领用并登记；
+            <br>
+            4、负责机票、火车票、汽车票、酒店预订工作。
+          </p>
+          <p>
+            <strong>岗位要求:</strong>
+            <br>
+            1、本科以上学历，中文、文秘及相关专业，文字表达能力强；
+            <br>
+            2、熟练使用WORD/PPT/EXCEL等办公软件；
+            <br>
+            3、熟悉电脑操作，应用表格、文档的操作；
+            <br>
+            4、形象气质好、善于沟通协调，有亲和力及团队合作精神。
+          </p>
+          <p>
+            <strong>简历投递邮箱:</strong>
+            <br>
+            <el-link href="https://www.baidu.com"
+                     target="_blank">lei.deng@cybernaut.com.cn</el-link>
+          </p>
+        </div>
+      </div>
+      <el-divider></el-divider>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      img: require("../assets/contact.png")
+    }
+  }
+}
+</script>
+<style scoped>
+.banner {
+  height: 380px;
+  background-image: url("../assets/contact.png");
+  background-repeat: no-repeat;
+  overflow: hidden;
+}
+.banner dl {
+  width: 317px;
+  height: 52px;
+  color: #fff;
+  margin: 0 auto;
+  margin-top: 150px;
+  overflow: hidden;
+}
+
+.banner dd {
+  height: 1px;
+  width: 60px;
+  background-color: #fff;
+  float: left;
+  margin-top: 25px;
+  margin-left: 0;
+}
+.banner dl dt {
+  font-size: 2.5em;
+  vertical-align: middle;
+  float: left;
+  margin: 0 17.5px;
+}
+.content {
+  max-width: 1200px;
+  padding: 20.3px;
+  margin: 0 auto;
+  padding-bottom: 50px;
+  background-color: #fff;
+  overflow: hidden;
+  margin-bottom: 50px;
+}
+.content h3 {
+  font-size: 1.75em;
+  font-weight: normal;
+  margin-bottom: 1em;
+  line-height: 1;
+  margin: 0;
+}
+.content .content-sub {
+  overflow: hidden;
+  padding-top: 35px;
+  line-height: 1;
+}
+.content .content-sub .sub1 {
+  float: left;
+  overflow: hidden;
+  margin-top: 6px;
+}
+.content .content-sub .sub1 h5 {
+  font-size: 1.333em;
+  font-weight: normal;
+  color: #06308d;
+  margin-bottom: 20px;
+  margin-top: 0px;
+}
+.content .content-sub .sub2 {
+  float: right;
+  line-height: 2.2;
+  overflow: hidden;
+  max-width: 500px;
+  margin-right: 50px;
+}
+.content .content-sub .sub2 p {
+  margin-top: 0;
+}
+.content .content-sub .sub2 p a {
+  text-decoration: none;
+  color: black;
+}
+</style>
+

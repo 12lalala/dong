6691915235618af93b38a0816a8e0e53b48821cd
LetSomebody <1144639044@qq.com>
2019-06-26 21:26:46 +0800

    增加Header内容

--- dong/src/components/Hello.vue
+++ dong/src/components/Hello.vue
@@ -13,7 +13,7 @@
         </el-carousel-item>
       </el-carousel>
     </div>
-    <!-- 卡片 -->
+    <!-- 分类 -->
     <div class="part1 content">
       <ul>
         <a href="/page/page13.html"
@@ -81,6 +81,51 @@
       </ul>
       <p class="clear"></p>
     </div>
+    <!-- 卡片 -->
+    <div class="cards">
+      <div class="card">
+        <h2>关于我们</h2>
+        <el-card class="box-card"
+                 :body-style="{ padding: '15px' }"
+                 shadow="hover">
+          <div>
+            <el-image style="width: 550px; height: 250px"
+                      :src="aboutimage"
+                      :fit="fit"></el-image>
+            <div>
+              <span>浙江大学国际创新研究院创是浙江大学下设的民办非企业研究院，
+                由赛伯乐投资集团筹资发起成立。作为国际性的创新创业平台，依托浙江大学和赛
+                伯乐的资源及资金优势，研究院以“锻造国际产学研合作创新链、助推创新生态营造与区域经济发展……</span><br>
+              <el-link href="/1"
+                       :underline="false"
+                       type="primary"
+                       style="float: right; padding: 3px 0">更多<i class="el-icon-d-arrow-right"></i></el-link>
+            </div>
+          </div>
+        </el-card>
+      </div>
+      <div class="card">
+        <h2>最新动态</h2>
+        <el-card class="box-card"
+                 :body-style="{ padding: '15px' }"
+                 shadow="hover">
+          <div>
+            <el-image style="width: 550px; height: 250px"
+                      :src="aboutimage"
+                      :fit="fit"></el-image>
+            <div>
+              <span>浙江大学国际创新研究院创是浙江大学下设的民办非企业研究院，
+                由赛伯乐投资集团筹资发起成立。作为国际性的创新创业平台，依托浙江大学和赛
+                伯乐的资源及资金优势，研究院以“锻造国际产学研合作创新链、助推创新生态营造与区域经济发展……</span><br>
+              <el-link href="/10"
+                       :underline="false"
+                       type="primary"
+                       style="float: right; padding: 3px 0">更多<i class="el-icon-d-arrow-right"></i></el-link>
+            </div>
+          </div>
+        </el-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -92,13 +137,16 @@ export default {
       bannerH: 343,
       imgList: [
         { url: require("../assets/hello.jpg") },
+        { url: require("../assets/hello.jpg") },
+        { url: require("../assets/hello.jpg") }
       ],
       imgCard: [
         require("../assets/imgcard1.png"),
         require("../assets/imgcard2.png"),
         require("../assets/imgcard3.png"),
         require("../assets/imgcard4.png")
-      ]
+      ],
+      aboutimage: require("../assets/about_pic.png")
     }
   },
   methods: {
@@ -154,7 +202,7 @@ a {
 .router-link-active {
   text-decoration: none;
 }
-/* 卡片 */
+/* 分类 */
 ul,
 li,
 ol {
@@ -221,45 +269,39 @@ a:visited {
   transform: scale(1.1);
   transition: all 0.3s;
 }
+/* 卡片 */
+.cards {
+  max-width: 1200px;
+  margin: 20px auto;
+  overflow: hidden;
+}
+.card {
+  max-width: 600px;
+  margin: 20px 5px auto;
+  float: left;
+}
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 580px;
+}
 @media only screen and (max-width: 1000px) and (min-width: 830px) {
-  /* 跑马灯 */
-  .carousel {
-    overflow: hidden;
-  }
-
-  .img1 {
-    text-align: center;
-    display: table-cell;
-    vertical-align: middle;
-  }
-  .img2 {
-    max-width: 100%;
-    max-height: 100%;
-  }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
-  }
-  /* 清除rooter-link的样式 */
-  a {
-    color: #ffffff;
-    text-decoration: none;
-  }
-  .router-link-active {
-    text-decoration: none;
-  }
-  /* 卡片 */
+  /* 分类 */
   ul,
   li,
   ol {
@@ -328,44 +370,7 @@ a:visited {
   }
 }
 @media only screen and (max-width: 830px) {
-  /* 跑马灯 */
-  .carousel {
-    overflow: hidden;
-  }
-
-  .img1 {
-    text-align: center;
-    display: table-cell;
-    vertical-align: middle;
-  }
-  .img2 {
-    max-width: 100%;
-    max-height: 100%;
-  }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
-  }
-  /* 清除rooter-link的样式 */
-  a {
-    color: #ffffff;
-    text-decoration: none;
-  }
-  .router-link-active {
-    text-decoration: none;
-  }
-  /* 卡片 */
+  /* 分类 */
   ul,
   li,
   ol {
